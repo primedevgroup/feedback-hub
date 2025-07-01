e99b45b0bc833a3877fc08108d03c2140f36d8a4
Davi Rezende <daviresendes12@gmail.com>
2025-06-30 21:45:10 -0300

    feat: squad page layout

--- src/app/(main)/squads/page.tsx
+++ src/app/(main)/squads/page.tsx
@@ -1,3 +1,56 @@
+import { Plus } from 'lucide-react'
+
+import { CreateSquadDialog } from '@/components/create-squad-dialog'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { UserTag } from '@/components/user-tag'
+
 export default function SquadPage() {
-  return <h1>My squads</h1>
+  return (
+    <div className="flex h-full w-full flex-col">
+      <div className="grid w-full grid-cols-[repeat(auto-fit,minmax(360px,1fr))] items-stretch gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Development Team PrimeDev</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <UserTag className="w-min">6</UserTag>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Development Team PrimeDev</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <UserTag className="w-min">6</UserTag>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Development Team PrimeDev</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <UserTag className="w-min">6</UserTag>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Development Team PrimeDev</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <UserTag className="w-min">6</UserTag>
+          </CardContent>
+        </Card>
+
+        <CreateSquadDialog>
+          <Button
+            variant="outline"
+            className="h-full min-h-[126px] rounded-2xl"
+          >
+            <Plus />
+          </Button>
+        </CreateSquadDialog>
+      </div>
+    </div>
+  )
 }
---
+++ src/components/create-squad-dialog.tsx
@@ -0,0 +1,54 @@
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Plus } from 'lucide-react'
+import { ComponentProps } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Input } from './form/input'
+import { Button } from './ui/button'
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from './ui/dialog'
+import { Form } from './ui/form'
+
+const squadSchema = z.object({
+  name: z
+    .string({ required_error: 'What is the name of your squad?' })
+    .min(3, 'You can do better!'),
+})
+
+type SquadData = z.infer<typeof squadSchema>
+
+export function CreateSquadDialog({
+  children,
+  ...props
+}: ComponentProps<typeof Dialog>) {
+  const form = useForm<SquadData>({
+    resolver: zodResolver(squadSchema),
+  })
+
+  async function createSquad(data: SquadData) {
+    console.log(data)
+  }
+
+  return (
+    <Dialog {...props}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent>
+        <DialogTitle>Create a squad</DialogTitle>
+        <Form {...form}>
+          <form
+            className="flex w-full flex-col gap-4"
+            onSubmit={form.handleSubmit(createSquad)}
+          >
+            <Input name="name" label="Squad name:" />
+
+            <Button>
+              <Plus />
+              Create squad
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  )
+}
